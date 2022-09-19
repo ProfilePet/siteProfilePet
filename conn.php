f1fed1f3161e60ecdb1c20913f0aef8fbfd7fcd8
Nicolas Rodrigues de Campos <nicolasrod1411@gmail.com>
2022-09-19 16:57:03 -0300

    Estrutura de Base

---
+++ conn.php
@@ -0,0 +1,17 @@
+<?php
+    $host = "";
+    $user = "";
+    $pass = "";
+    $banco = "";
+
+    try{
+    $pdo = new PDO ("mysql:dbname=$banco;host=$host","$user","$pass");
+    }
+    catch(PDOException $e){
+        echo("Falha ao Conectar".$e->getMessage());
+    }
+    catch(Exception $e){
+        echo("Falha ao Conectar".$e->getMessage());
+    }
+
+?>
