905971320bf45a63b96e0557e2db19b056edf1f9
Nicolas Rodrigues de Campos <nicolasrod1411@gmail.com>
2022-10-12 17:02:15 -0300

    1210

--- DAO/DiagnosticoDAO.php
+++ DAO/DiagnosticoDAO.php
@@ -14,11 +14,10 @@ class DiagnosticoDAO{
 
     public static function editarDiagnostico(Diagnostico $dia){
         include ('conn.php');
-        $retornoDB = $pdo->prepare("UPDATE tbdiagnostico SET tratamento = :t, codAnimal = :ca, codEnfermidade = :ce, ativoDiagnostico = :a WHERE codDiagnostico = :cd");
+        $retornoDB = $pdo->prepare("UPDATE tbdiagnostico SET tratamento = :t, codAnimal = :ca, codEnfermidade = :ce WHERE codDiagnostico = :cd");
         $retornoDB->bindValue(":t", $dia->getTratamento());
         $retornoDB->bindValue(":ca", $dia->getCodAnimal());
         $retornoDB->bindValue(":ce", $dia->getCodEnfermidade());
-        $retornoDB->bindValue(":a", $dia->getAtivoDiagnostico());
         $retornoDB->bindValue(":cd", $dia->getCodDiagnostico());
         $retornoDB->execute();
         return $retornoDB;
@@ -26,9 +25,16 @@ class DiagnosticoDAO{
 
     public static function consultarDiagnostico(Diagnostico $dia){
         include('conn.php');
-        $retornoDB = $pdo->prepare("SELECT codDiagnostico, tratamento, nomeEnfermidade, ativoDiagnostico FROM tbdiagnostico INNER JOIN tbEnfermidade ON tbdiagnostico.codEnfermidade = tbEnfermidade.codEnfermidade WHERE codAnimal = :ca");
+        $retornoDB = $pdo->query("SELECT codDiagnostico, tratamento, nomeEnfermidade, ativoDiagnostico FROM tbdiagnostico INNER JOIN tbEnfermidade ON tbdiagnostico.codEnfermidade = tbEnfermidade.codEnfermidade WHERE codAnimal = :ca");
         $retornoDB->bindValue(":ca", $dia->getCodAnimal());
         $retornoDB->execute();
+        return $retornoDB;
+    }
+
+    public static function excluirDiagnostico(Diagnostico $dia){
+        include('conn.php');
+        $retornoDB = $pdo->prepare("UPDATE tbdiagnostico SET ativoDiagnostico = false WHERE codAnimal = :ca");
+        $retornoDB->bindValue(":ca", $an->getCodAnimal());
         return $retornoDB;
     }
 }
---
+++ DAO/MedicacaoDAO.php
@@ -0,0 +1,7 @@
+<?php
+class MedicacaoDAO{
+    public static function consultarMedicacao(Medicacao $med){
+        include('conn.php');
+        $retornoDB = $pdo->query("");
+    }
+}
