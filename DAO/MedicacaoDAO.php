0ec84b716d980831cb1703a3bc436fcbf00ab955
Etec <etec@etec.com>
2022-10-20 17:38:20 -0300

    2010

--- DAO/MedicacaoDAO.php
+++ DAO/MedicacaoDAO.php
@@ -1,7 +1,21 @@
 <?php
+include_once('Model/Medicacao.php');
 class MedicacaoDAO{
-    public static function consultarMedicacao(Medicacao $med){
+    public static function consultarMedicacao(){
         include('conn.php');
-        $retornoDB = $pdo->query("");
+        $retornoDB = $pdo->query("SELECT codMedicacao, nomeMedicacao FROM tbmedicacao ORDER BY nomeMedicacao");
+    }
+
+    public static function consultarMedicacaoDiag($codDiag){
+        include('conn.php');
+        $retornoDB = $pdo->prepare("SELECT * FROM tbmedicacaodiagnostico WHERE codDiagnostico = :c");
+        $retornoDB->bindValue(":c", $codDiag);
+        $retornoDB->execute();  
+        return $retornoDB;
+    }
+
+    public static function cadastrarMedicacaoDiag(Medicacao $med)
+    {
+        # code...
     }
 }
--- View/modulos/Animal/perfil.php
+++ View/modulos/Animal/perfil.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <?php
-        echo"$codAnimal";
+        //echo"$codAnimal";
     ?>
 </body>
 </html>
