2c678ad97c702ef48d67458f5cb5e808d36b1828
Nicolas Rodrigues de Campos <nicolasrod1411@gmail.com>
2022-10-24 17:35:44 -0300

    Create LembreteConsulta.php

---
+++ Model/LembreteConsulta.php
@@ -0,0 +1,15 @@
+<?php
+class LembreteConsulta{
+    private $codConsulta;
+    private $dataConsulta;
+    private $horaConsulta;
+    private $nomeClinica;
+    private $local;
+    private $nomeVeterinario;
+    private $tipoConsulta;
+    private $ativoConsulta;
+    private $codAnimal;
+    private $codDiagnostico;
+
+    
+}
