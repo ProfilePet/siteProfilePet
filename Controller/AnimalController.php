1ec70f88c9470ca85881aeb15a3b49a64b571107
Nicolas Rodrigues de Campos <nicolasrod1411@gmail.com>
2022-10-28 16:46:24 -0300

    2810

--- Controller/AnimalController.php
+++ Controller/AnimalController.php
@@ -1,49 +1,52 @@
 <?php
-    class AnimalController{
-        public function telaCadastrar(){
-            include('DAO/AnimalDAO.php');
-            //Consulta para a select de Especie
-            $consultaTemperamento = AnimalDAO::consultarTemperamento();
-            $consultaTemperamento = $consultaTemperamento->fetchAll();
-            $consultaEspecie = AnimalDAO::consultarEspecie();
-            $consultaEspecie = $consultaEspecie->fetchAll();
-            session_start();
-            $codUsu=$_SESSION['codUsuario'];
-            include('View/modulos/Animal/cadastro.php');
-            
-        }
-        public function cadastrar(){
-            include('Model/Animal.php');
-            include('DAO/AnimalDAO.php');
-            session_start();
-            $codUsu=$_SESSION['codUsuario'];
-            $objAnimal = new Animal();
-            $objAnimal->setNomeAnimal($_POST['txtNomeAnimal']);
-            $objAnimal->setNascimentoAnimal($_POST['txtCalendario']);
-            $objAnimal->setAtivoAnimal(1);
-            $objAnimal->setCodUsuarioA($codUsu);
-            $objAnimal->setCodRacaAnimal($_POST['txtRaca']);
-            $objAnimal->setTemperamentoAnimal($_POST['txtTemperamento']);
-            //Pegando Imagem do Pet e Renomeando e Mando Para Pasta Imagens/PetPhoto
-            if(isset($_FILES['txtImagem']))
-            {   
-                $arquivo = $_FILES["txtImagem"]["name"];
-                $tempname = $_FILES["txtImagem"]["tmp_name"];             
-                $extensao = pathinfo($arquivo,PATHINFO_EXTENSION);
-                $pasta = "Imagens/PetPhoto/"."{$objAnimal->getCodUsuarioA()}$arquivo";
+class AnimalController
+{
+    public function telaCadastrar()
+    {
+        include('DAO/AnimalDAO.php');
+        //Consulta para a select de Especie
+        $consultaTemperamento = AnimalDAO::consultarTemperamento();
+        $consultaTemperamento = $consultaTemperamento->fetchAll();
+        $consultaEspecie = AnimalDAO::consultarEspecie();
+        $consultaEspecie = $consultaEspecie->fetchAll();
+        session_start();
+        $codUsu = $_SESSION['codUsuario'];
+        include('View/modulos/Animal/cadastro.php');
+    }
+    public function cadastrar()
+    {
+        include('Model/Animal.php');
+        include('DAO/AnimalDAO.php');
 
-                    if (move_uploaded_file($tempname, $pasta))  {
-                        echo "Image uploaded successfully";
-                        $objAnimal->setImagemAnimal($pasta);
+        session_start();
+        $codUsu = $_SESSION['codUsuario'];
 
-                    }else{
-                        echo "Failed to upload image";
-                        $objAnimal->setImagemAnimal('Imagens/PetPhoto/test.jpg');
-                }
+        $objAnimal = new Animal();
+        $objAnimal->setNomeAnimal($_POST['txtNomeAnimal']);
+        $objAnimal->setNascimentoAnimal($_POST['txtCalendario']);
+        $objAnimal->setAtivoAnimal(1);
+        $objAnimal->setCodUsuarioA($codUsu);
+        $objAnimal->setCodRacaAnimal($_POST['txtRaca']);
+        $objAnimal->setTemperamentoAnimal($_POST['txtTemperamento']);
+
+        //Pegando Imagem do Pet e Renomeando e Mando Para Pasta Imagens/PetPhoto
+        if (isset($_FILES['txtImagem'])) {
+            $arquivo = $_FILES["txtImagem"]["name"];
+            $tempname = $_FILES["txtImagem"]["tmp_name"];
+            $extensao = pathinfo($arquivo, PATHINFO_EXTENSION);
+            $pasta = "Imagens/PetPhoto/" . "{$objAnimal->getCodUsuarioA()}$arquivo";
+
+            if (move_uploaded_file($tempname, $pasta)) {
+                echo "Image uploaded successfully";
+                $objAnimal->setImagemAnimal($pasta);
+            } else {
+                echo "Failed to upload image";
+                $objAnimal->setImagemAnimal('Imagens/PetPhoto/test.jpg');
             }
-            //var_dump($objAnimal);
-            $retorno = AnimalDAO::cadastrar($objAnimal);
-            echo "
+        }
+
+        $retorno = AnimalDAO::cadastrar($objAnimal);
+        echo "
             <body></body><script src=//cdn.jsdelivr.net/npm/sweetalert2@11></script>
                     <script type=\"text/javascript\">
                     Swal.fire({
@@ -65,60 +68,61 @@
                     })
                     </script>
                     ";
-        }
-        public function telaConsultar(){
-            include('DAO/AnimalDAO.php');
-            session_start();
-            $codUsu=$_SESSION['codUsuario'];
-            $consultaAnimais = AnimalDAO::consultarMenu($codUsu);
-            $consultaAnimais = $consultaAnimais->fetchAll();
-            include('View/modulos/Animal/consulta.php');
-        }
-        public function telaEditar($cod){
-            include('Model/Animal.php');
-            include_once('DAO/AnimalDAO.php');
-            $consultaAnimal = AnimalDAO::consultarPerfil($cod);
-            $consultaAnimal = $consultaAnimal->fetchAll();
-            $consultaEspecie = AnimalDAO::consultarEspecie();
-            $consultaEspecie = $consultaEspecie->fetchAll();
-            //var_dump($consultaAnimal);
-            include('View/modulos/Animal/editar.php');
+    }
 
-        }
+    public function telaConsultar()
+    {
+        include('DAO/AnimalDAO.php');
+        session_start();
+        $codUsu = $_SESSION['codUsuario'];
+        $consultaAnimais = AnimalDAO::consultarMenu($codUsu);
+        $consultaAnimais = $consultaAnimais->fetchAll();
+        include('View/modulos/Animal/consulta.php');
+    }
+
+    public function telaEditar($cod)
+    {
+        include('Model/Animal.php');
+        include_once('DAO/AnimalDAO.php');
+        $consultaAnimal = AnimalDAO::consultarPerfil($cod);
+        $consultaAnimal = $consultaAnimal->fetchAll();
+        $consultaEspecie = AnimalDAO::consultarEspecie();
+        $consultaEspecie = $consultaEspecie->fetchAll();
+        //var_dump($consultaAnimal);
+        include('View/modulos/Animal/editar.php');
+    }
+
+    public function editar()
+    {
+        include('Model/Animal.php');
+        include_once('DAO/AnimalDAO.php');
+        session_start();
+        $objAnimal = new Animal();
+        $objAnimal->setCodAnimal($_SESSION['codAnimal']);
+        $objAnimal->setNomeAnimal($_POST['txtNomeAnimal']);
+        $objAnimal->setCodRacaAnimal($_POST['txtRaca']);
+        $objAnimal->setNascimentoAnimal($_POST['txtCalendario']);
+        $objAnimal->setAtivoAnimal(1);
+        $objAnimal->setTemperamentoAnimal(1);
         
-        public function editar(){
-            include('Model/Animal.php');
-            include_once('DAO/AnimalDAO.php');
-            session_start();
-            $objAnimal = new Animal();
-            $objAnimal->setCodAnimal($_SESSION['codAnimal']);
-            $objAnimal->setNomeAnimal($_POST['txtNomeAnimal']);
-            $objAnimal->setCodRacaAnimal($_POST['txtRaca']);
-            $objAnimal->setNascimentoAnimal($_POST['txtCalendario']);
-            $objAnimal->setAtivoAnimal(1);
-            $objAnimal->setTemperamentoAnimal(1);
-            if (isset($_POST['btnExcluir'])){
-                $retorno =  AnimalDAO::deletarAnimal($objAnimal);
-                echo "
+        if (isset($_POST['btnExcluir'])) {
+            $retorno =  AnimalDAO::deletarAnimal($objAnimal);
+            echo "
                 <script type=\"text/javascript\">
                 alert(\"Animal Excluido com Sucesso.\");
                 window.location='tela-consulta-animal';
                 </script> ";
-            }
-            else{
-            if(isset($_FILES['txtImagem']))
-            {   
+        } else {
+            if (isset($_FILES['txtImagem'])) {
                 $arquivo = $_FILES["txtImagem"]["name"];
-                $tempname = $_FILES["txtImagem"]["tmp_name"];             
-                $extensao = pathinfo($arquivo,PATHINFO_EXTENSION);
-                $pasta = "Imagens/PetPhoto/"."{$objAnimal->getCodUsuarioA()}$arquivo";
+                $tempname = $_FILES["txtImagem"]["tmp_name"];
+                $extensao = pathinfo($arquivo, PATHINFO_EXTENSION);
+                $pasta = "Imagens/PetPhoto/" . "{$objAnimal->getCodUsuarioA()}$arquivo";
 
-                    if (move_uploaded_file($tempname, $pasta))  {
-                        $objAnimal->setImagemAnimal($pasta);
-                       
-
-                    }else{
-                        $objAnimal->setImagemAnimal($_SESSION['imagemAnimal']);
+                if (move_uploaded_file($tempname, $pasta)) {
+                    $objAnimal->setImagemAnimal($pasta);
+                } else {
+                    $objAnimal->setImagemAnimal($_SESSION['imagemAnimal']);
                 }
             }
             $retorno = AnimalDAO::editarAnimal($objAnimal);
@@ -128,26 +132,28 @@
                     window.location='tela-consulta-animal';
                     </script>
                     ";
-                }
-        }
-        public function telaPerfil($cod){
-            include_once('DAO/AnimalDAO.php');
-            $consultaAnimal = AnimalDAO::consultarPerfil($cod);
-            $consultaAnimal = $consultaAnimal->fetchAll();
-            var_dump($consultaAnimal);
-            include('View/modulos/Animal/perfil.php');
-        }
-        public function edita_Consulta(){
-            if (isset($_POST['btAnimal'])){
-                echo"Consultar";
-                $cod=$_POST['btAnimal'];
-                $this->telaPerfil($cod);
-            }
-            else if (isset($_POST['btEditar'])){
-                echo"Editar";
-                $cod=$_POST['btEditar'];
-                $this->telaEditar($cod);
-            }
         }
     }
-?>
+
+    public function telaPerfil($cod)
+    {
+        include_once('DAO/AnimalDAO.php');
+        $consultaAnimal = AnimalDAO::consultarPerfil($cod);
+        $consultaAnimal = $consultaAnimal->fetchAll();
+        var_dump($consultaAnimal);
+        include('View/modulos/Animal/perfil.php');
+    }
+
+    public function edita_Consulta()
+    {
+        if (isset($_POST['btAnimal'])) {
+            echo "Consultar";
+            $cod = $_POST['btAnimal'];
+            $this->telaPerfil($cod);
+        } else if (isset($_POST['btEditar'])) {
+            echo "Editar";
+            $cod = $_POST['btEditar'];
+            $this->telaEditar($cod);
+        }
+    }
+}
--- DAO/AnimalDAO.php
+++ DAO/AnimalDAO.php
@@ -1,13 +1,12 @@
 <?php
 include_once('Model/Animal.php');
-$tabela = 'tbanimal';
-class AnimalDAO {
-    public static function cadastrar(Animal $an){
-        include ('conn.php');
-
-        $retornoDB = $pdo->prepare("INSERT INTO tbanimal (nomeAnimal,imagemAnimal,nascimentoAnimal,ativoAnimal,codUsuario,codRacaAnimal,codTemperamento) 
-                                    VALUES (:n,:i,:na,:a,:c,:r,:t)");
-        echo"a";                
+class AnimalDAO
+{
+    //Insere as informações do animal no Banco
+    public static function cadastrar(Animal $an)
+    {
+        include('conn.php');
+        $retornoDB = $pdo->prepare("INSERT INTO tbanimal (nomeAnimal,imagemAnimal,nascimentoAnimal,ativoAnimal,codUsuario,codRacaAnimal,codTemperamento) VALUES (:n,:i,:na,:a,:c,:r,:t)");
         $retornoDB->bindValue(":n", $an->getNomeAnimal());
         $retornoDB->bindValue(":i", $an->getImagemAnimal());
         $retornoDB->bindValue(":na", $an->getNascimentoAnimal());
@@ -15,56 +14,61 @@ class AnimalDAO {
         $retornoDB->bindValue(":c", $an->getCodUsuarioA());
         $retornoDB->bindValue(":r", $an->getCodRacaAnimal());
         $retornoDB->bindValue(":t", $an->getTemperamentoAnimal());
-        echo"a";
         $retornoDB->execute();
-        
         return $retornoDB;
     }
 
-    public static function consultarMenu($cod){
-        include ('conn.php');
+    //Consulta nome, código, e imagem do animal para ser utilizada no Menu de Animais
+    public static function consultarMenu($cod)
+    {
+        include('conn.php');
         $retornoDB = $pdo->prepare("SELECT codAnimal, nomeAnimal, imagemAnimal FROM tbanimal WHERE codUsuario = :c AND ativoAnimal = 1");
         $retornoDB->bindValue(":c", $cod);
         $retornoDB->execute();
-
         return $retornoDB;
     }
-    public static function consultarEspecie(){
-        include ('conn.php');
-        $retornoDB = $pdo->query("SELECT * FROM tbespecie /*WHERE codUsuario = $*/");
-        //$retornoDB->bindValue(":c", $c);
 
+    //Consulta todas especies da tabela padrão
+    public static function consultarEspecie()
+    {
+        include('conn.php');
+        $retornoDB = $pdo->query("SELECT * FROM tbespecie");
         return $retornoDB;
     }
-    public static function consultarRaca($esp){
-        include ('../conn.php');
+
+    //Consulta todas as Raças com base na espécie pré escolhida pelo usuario;
+    public static function consultarRaca($esp)
+    {
+        include('../conn.php');
         $retornoDB = $pdo->prepare("SELECT * FROM tbraca WHERE codEspecieAnimal = :es");
         $retornoDB->bindValue(":es", $esp);
         $retornoDB->execute();
-
-        return $retornoDB;
-    }
-    public static function consultarTemperamento(){
-        include ('conn.php');
-        $retornoDB = $pdo->query("SELECT * FROM tbtemperamento /*WHERE codUsuario = $*/");
-        //$retornoDB->bindValue(":c", $c);
-
         return $retornoDB;
     }
 
-    public static function consultarPerfil($cod){
-        require ('conn.php');
-        $retornoDB = $pdo->prepare("SELECT * FROM tbanimal WHERE codAnimal = :ca");
+    //Consulta todos os Temperamentos da tabela padrão
+    public static function consultarTemperamento()
+    {
+        include('conn.php');
+        $retornoDB = $pdo->query("SELECT * FROM tbtemperamento");
+        return $retornoDB;
+    }
+
+    //Consulta todos os Dados do Animal para ser colocado no Perfil do Animal
+    public static function consultarPerfil($cod)
+    {
+        require('conn.php');
+        $retornoDB = $pdo->prepare("SELECT nomeAnimal, imagemAnimal, nascimentoAnimal, nomeRacaAnimal, temperamento FROM tbanimal INNER JOIN tbRaca ON tbRaca.codRaca = tbAnimal.codRaca INNER JOIN tbTemperamento ON tbAnimal.codTemperamento = tbTemperamento.codTemperamento WHERE codAnimal = :ca");
         $retornoDB->bindValue(":ca", $cod);
         $retornoDB->execute();
-
         return $retornoDB;
     }
 
-    public static function editarAnimal(Animal $an){//Edita os Atributos do Animal no Banco
+    //Edita os Atributos do Animal no Banco
+    public static function editarAnimal(Animal $an)
+    { 
         require_once('conn.php');
-        $retornoDB = $pdo->prepare("UPDATE tbanimal SET nomeAnimal = :n, imagemAnimal = :i, nascimentoAnimal = :na,
-                                    codRacaAnimal = :r, codTemperamento = :t WHERE codAnimal = :ca");
+        $retornoDB = $pdo->prepare("UPDATE tbanimal SET nomeAnimal = :n, imagemAnimal = :i, nascimentoAnimal = :na, codRacaAnimal = :r, codTemperamento = :t WHERE codAnimal = :ca");
         $retornoDB->bindValue(":n", $an->getNomeAnimal());
         $retornoDB->bindValue(":i", $an->getImagemAnimal());
         $retornoDB->bindValue(":na", $an->getNascimentoAnimal());
@@ -75,12 +79,13 @@ class AnimalDAO {
         return $retornoDB;
     }
 
-    public static function deletarAnimal(Animal $an){//Inativa o Perfil do Usuário no Banco
+    //Inativa o Perfil do Usuário no Banco
+    public static function deletarAnimal(Animal $an)
+    {
         require_once('conn.php');
         $retornoDB = $pdo->prepare("UPDATE tbanimal SET ativoAnimal = 0 WHERE codAnimal = :ca");
         $retornoDB->bindValue(":ca", $an->getCodAnimal());
         $retornoDB->execute();
-
         return $retornoDB;
     }
 }
